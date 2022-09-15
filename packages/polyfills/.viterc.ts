7f411b8b16df8b9949fda80bdaa42d6d5f83473d
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-09-14 19:35:30 -0500

    fix: fixed lint command

--- packages/polyfills/.viterc.ts
+++ packages/polyfills/.viterc.ts
@@ -4,10 +4,7 @@ import {defineConfig} from '@micra/vite-config/library';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: [
-        '@remix-run/web-fetch',
-        '@remix-run/web-file',
-      ],
+      external: ['@remix-run/web-fetch', '@remix-run/web-file'],
       input: {
         index: cwd('index.ts'),
         fetch: cwd('fetch.ts'),
